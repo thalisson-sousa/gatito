1f1bf808454d4c3b1cfe629aea99c50710cbae4f
Thalisson <thalissonpop91@gmail.com>
2024-01-26 12:04:01 -0300

    feat: first commit

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import Carrinho from "./src/screens/Carrinho";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Carrinho />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
